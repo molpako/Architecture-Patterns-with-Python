21fb9d2da93f76706b46eefbea9647c948737c14
sakaok <sakaok@pepabo.com>
2024-07-21 00:52:04 +0900

    success test_can_only_deallocate_allocated_lines

--- part1/repository_pattern/model.py
+++ part1/repository_pattern/model.py
@@ -14,14 +14,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
 
-    def allocate(self, line: OrderLine):
-        # ドメインモデルの場合、期待される引数が何であるかを明確にしたり、文書化するのに役立つことがあります。 読みやすさの点で支払う代償は高すぎると判断するかもしれません。
-        self.available_quantity -= line.qty
+    def allocate(self, line: OrderLine) -> None:
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
-
-    def deallocate(self, line: OrderLine):
-        self.available_quantity += line.qty
