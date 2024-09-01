b6129b81d7214ec1df7c1cd849f21ef5e14b896f
sakaok <sakaok@pepabo.com>
2024-09-01 17:12:11 +0900

    part9: Prepare events to make everything an event handler

--- part9/domain/events.py
+++ part9/domain/events.py
@@ -1,8 +1,30 @@
+from datetime import date
 from dataclasses import dataclass
 
 
 class Event:
     pass
+
+
+@dataclass
+class BatchCreated(Event):
+    ref: str
+    sku: str
+    qty: int
+    eta: date | None = None
+
+
+@dataclass
+class BatchQuantityChanged(Event):
+    ref: str
+    qty: int
+
+
+@dataclass
+class AllocationRequired(Event):
+    orderid: str
+    sku: str
+    qty: int
 
 
 @dataclass
